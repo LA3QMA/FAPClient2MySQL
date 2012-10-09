0bf12d9e8b67d3787b67873bba1589af6209d510
LA3QMA <kai.gunter.brandt@gmail.com>
2012-10-09 14:10:26 +0200

    Now insert data to APRSPackets

--- Client2SQL.pl
+++ Client2SQL.pl
@@ -13,11 +13,11 @@ $database = "APRSC";
 $host = "localhost";
 $tablename = "APRSPackest";
 $user = "APRS";
-$pw = "password";
+$pw = "";
 $port = "3306";
 
 # APRS-IS config
-$IShost = "hostname:14580";
+$IShost = "servername:14580";
 $ISmycall = "N0CALL";
 $ISfilter = "t/poimqstunw";
 $ISclient = "APRSC_MySQL_Client 1.0";
@@ -31,51 +31,78 @@ for (;;){
         my $l = $is->getline_noncomment();
         next if (!defined $l);
 
-        print "\n--- new packet ---\n";
-
         my %packetdata;
         my $retval = parseaprs($l, \%packetdata);
 
         if ($retval == 1) {
 # Comment out 'print' as this is only here for debug information
 # Should probably add a 'verbose' function later
-                        print "CALLSIGN: $packetdata{srccallsign}\n";
-                        print "TYPE: $packetdata{type}\n";
-                        print "PACKET: $packetdata{origpacket}\n";
-                        print "LATITUDE: $packetdata{latitude}\n";
-                        print "LONGITUDE: $packetdata{longitude}\n";
-                        print "SYMBOLtable: $packetdata{symboltable}\n";
-                        print "SYMBOL: $packetdata{symbolcode}\n";
-                        print "SPEED: $packetdata{speed}\n";
-                        print "COURSE: $packetdata{course}\n";
-                        print "ALTITUDE: $packetdata{altitude}\n";
-                        print "BODY: $packetdata{body}\n";
+#                        print "CALLSIGN: $packetdata{srccallsign}\n";
+#                        print "TYPE: $packetdata{type}\n";
+#                        print "PACKET: $packetdata{origpacket}\n";
+#                        print "LATITUDE: $packetdata{latitude}\n";
+#                        print "LONGITUDE: $packetdata{longitude}\n";
+#                        print "SYMBOLtable: $packetdata{symboltable}\n";
+#                        print "SYMBOL: $packetdata{symbolcode}\n";
+#                        print "SPEED: $packetdata{speed}\n";
+#                        print "COURSE: $packetdata{course}\n";
+#                        print "ALTITUDE: $packetdata{altitude}\n";
+#                        print "BODY: $packetdata{body}\n";
 
-                        if($packetdata{type} eq 'location')
-                        {
-                        ($Second, $Minute, $Hour, $DayOfMonth, $Month, $Year, $Weekday, $DayOfYear, $IsDST) = localtime(time);
-                        $RealMonth = $Month + 1;
+# Prepare to insert into APRSPackets
+# PacketType:
+# 0 = not identified
+# 1 = Message/Bulletin
+# 2 = Object/Item
+# 3 = position
+
+# Data needed: CallsignSSID, ReportTime, PacketType, IsWx, Packet
+# Going to add: StatusReport,StationCapabilities,
+			print "$packetdata{srccallsign}\n";
                         $GMTTime = gmtime(time);
                         $Time = &UnixDate($GMTTime, '%Y-%m-%d %H:%M:%S');
+#			print "$Time\n";
+#PacketType
+			if($packetdata{type} eq 'message')
+			{
+#			print "Message/Bulletin: 1\n";
+			$Ptype = 1;
+			}
+			if ($packetdata{type} eq 'object')
+			{
+#			print "Object/Item: 2 - Object\n";
+			$Ptype = 2;
+			}
+			if ($packetdata{type} eq 'item')
+			{
+#			print "Object/Item: 2 - Item\n";
+			$Ptype = 2;
+			}
+			if($packetdata{type} eq 'location')
+			{
+#			print "Position: 3\n";
+			$Ptype = 3;
+			} elsif ($packetdata{type} eq '!')
+			{
+#			print "Position: 3b\n";
+			$Ptype = 3;
+			}
+			if($packetdata{type} eq 'wx')
+			{
+			$IsWx = 1;
+			} else {
+			$IsWx = 0;
+			}
 
 	# Commented out until we know how and what we want to store in SQL
 
-	#$sqlquery = "INSERT INTO APRSPackets VALUES (\'" . $packetdata{srccallsign} . "\',\'" . $Time . "\',1,1,\'" . $packetdata{origpacket} . "\')";
-	#$query = $connect->prepare($sqlquery);
-	#$query->execute();
+	$sqlquery = "INSERT INTO APRSPackets VALUES (\'" . $packetdata{srccallsign} . "\',\'" . $Time . "\',\'" . $Ptype . "\',\'" . $IsWx . "\',\'" . $packetdata{origpacket} . "\')";
+print $sqlquery;
+print "\n";
+	$query = $connect->prepare($sqlquery);
+	$query->execute();
 	#printf('%d-%02d-%02d %02d:%02d:%02d', $Year+1900, $RealMonth, $DayOfMonth, $Hour, $Minute, $Second);
 
-                        } elsif($packetdata{type} eq 'object')
-                        {
-                        print "Debug: object";
-                        } elsif($packetdata{type} eq 'item')
-                        {
-                        print "Debug: item";
-                        } elsif($packetdata{type} eq 'message')
-                        {
-                        print "Debug: message";
-                        print "BODY: $packetdata{body}\n";
-                        }
         } else {
                 warn "Parsing failed: $packetdata{resultmsg} ($packetdata{resultcode})\n";
         }
